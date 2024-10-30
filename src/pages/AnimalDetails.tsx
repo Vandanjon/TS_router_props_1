7d9ea2a240b678cba554794544453960c8010e6b
Benoît Vandanjon <vandanjon@gmail.com>
2024-10-30 09:42:12 +0100

    [feat] : add two basic pages for routing

---
+++ src/pages/AnimalDetails.tsx
@@ -0,0 +1,9 @@
+export default function AnimalDetails() {
+  return (
+    <div id="AnimalDetailsContainer">
+      <h1>AnimalDetails</h1>
+
+      <p>AnimalDetails Page</p>
+    </div>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div id="HomeContainer">
+      <h1>Home</h1>
+
+      <p>Home Page</p>
+    </div>
+  );
+}
