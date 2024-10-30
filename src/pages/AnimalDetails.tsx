3ca394e83d67e0fc8526341ac7f2bd77515e267f
Benoît Vandanjon <vandanjon@gmail.com>
2024-10-30 10:00:52 +0100

    [feat] : change AnimalDtails page + style

--- src/pages/AnimalDetails.tsx
+++ src/pages/AnimalDetails.tsx
@@ -1,9 +1,37 @@
-export default function AnimalDetails() {
-  return (
-    <div id="AnimalDetailsContainer">
-      <h1>AnimalDetails</h1>
+import { useParams } from "react-router-dom";
+import { Animal } from "../utils/types";
+import animals from "../datas/datas.json";
 
-      <p>AnimalDetails Page</p>
+export default function AnimalDetails() {
+  const { id } = useParams<{ id: string }>();
+
+  function findAnimalById(id: string): Animal | undefined {
+    return animals.find((animal) => animal.id === Number(id));
+  }
+
+  const foundAnimal = id ? findAnimalById(id) : undefined;
+
+  if (!foundAnimal) {
+    return <div>Loading...</div>;
+  }
+
+  const { name, image, description, anecdote } = foundAnimal;
+
+  return (
+    <div id="animal-details-container">
+      <h1>{name} details</h1>
+
+      <section>
+        <figure>
+          <img src={image} alt={name} />
+        </figure>
+
+        <div id="infos">
+          <p>{description}</p>
+
+          <p>{anecdote}</p>
+        </div>
+      </section>
     </div>
   );
 }
