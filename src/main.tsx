3c891e224ded33f23b6598a666c9bd642ba9fadb
Benoît Vandanjon <vandanjon@gmail.com>
2024-10-30 09:45:46 +0100

    [feat] : add routes for Home and AnimalDetails pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot, Root } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  RouteObject,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Home from "./pages/Home";
+import AnimalDetails from "./pages/AnimalDetails";
+
+import "./styles/main.css";
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/animal/:id",
+    element: <AnimalDetails />,
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+const rootElement: HTMLElement | null = document.getElementById("root");
+
+if (rootElement) {
+  const root: Root = createRoot(rootElement);
+
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+} else {
+  console.error("Root element not found");
+}
